b84097bf6e0e5917014b6475e40ffc077fed613f
Stanislav Urasov <stanislav.urasov@apriorit.com>
2024-11-18 23:30:24 +0200

    added IPs output to file

--- HW9/output.tf
+++ HW9/output.tf
@@ -2,3 +2,14 @@ output "public_ip_addresses" {
   description = "Public IP addresses of the virtual machines"
   value       = [for ip in azurerm_public_ip.pip : ip.ip_address]
 }
+
+locals {
+  description = "Public IP addresses of the virtual machines for jenkins"
+    ips = templatefile("${path.module}/template.tpl", {
+        dd_ip = [for ip in azurerm_public_ip.pip : ip.ip_address] 
+    })
+}
+
+output "neededForAnsible" {
+  value = local.ips
+}
