9506fc20510a4412f13190cf71975202dd7110f1
Stanislav Urasov <stanislav.urasov@apriorit.com>
2024-11-22 14:23:25 +0200

    added tfstate storage

--- HW9/presets/provider.tf
+++ HW9/presets/provider.tf
@@ -1,3 +1,12 @@
 provider "azurerm" {
   features {}
 }
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "hw9-state"
+    storage_account_name = "ustashw9storacc"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
--- HW9/provider.tf
+++ HW9/provider.tf
@@ -2,3 +2,12 @@ provider "azurerm" {
   features {}
   subscription_id = "00963533-2602-4db1-9d9e-748b36f03a13"
 }
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "hw9-state"
+    storage_account_name = "ustashw9storacc"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
